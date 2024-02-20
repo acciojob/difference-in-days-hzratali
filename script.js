ec4736c758909e2a90d2a44cd5c01aefa2f9902a
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-02-20 23:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const oneDayMilliseconds = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+    const firstDate = Date.UTC(
+        parseInt(date1.substring(0, 4)), // Year
+        parseInt(date1.substring(5, 7)) - 1, // Month (zero-based)
+        parseInt(date1.substring(8, 10)) // Day
+    );
+    const secondDate = Date.UTC(
+        parseInt(date2.substring(0, 4)), // Year
+        parseInt(date2.substring(5, 7)) - 1, // Month (zero-based)
+        parseInt(date2.substring(8, 10)) // Day
+    );
+
+    const differenceMilliseconds = secondDate - firstDate;
+    const differenceDays = Math.floor(differenceMilliseconds / oneDayMilliseconds);
+    return differenceDays;
 };
 
 // Do not change the code below.
